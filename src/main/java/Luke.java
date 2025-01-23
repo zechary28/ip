aefbcce62a867a035482e54d3e526d5eaf4e1629
zechary28 <zechary28@gmail.com>
2025-01-23 14:21:01 +0800

    added gitignore and changed name to Luke

---
+++ src/main/java/Luke.java
@@ -0,0 +1,10 @@
+public class Luke {
+    public static void main(String[] args) {
+        String logo = " _           _        \n"
+                + "| |    _   _| | _____ \n"
+                + "| |   | | | | |/ / _ \\\n"
+                + "| |___| |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
