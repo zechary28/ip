fdc6ca77b2c869a79b4fee4dae2bab1489aa7218
zechary28 <zechary28@gmail.com>
2025-01-23 14:56:10 +0800

    added printLine and exit functions

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,5 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Luke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //io
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out);
+
         String logo = " _           _        \n"
                 + "| |    _   _| | _____ \n"
                 + "| |   | | | | |/ / _ \\\n"
@@ -8,8 +17,21 @@ public class Luke {
         System.out.println("_____________________________________");
         System.out.println("Hello I'm\n" + logo);
         System.out.println("What can I do for you?");
+
+        while (true) {
+            String input = reader.readLine();
+            if (input.equals("bye")) exit();
+            else System.out.println(input);
+        }
+    }
+
+    public static void printLine() {
         System.out.println("_____________________________________");
+    }
+
+    public static void exit() {
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________");
+        printLine();
     }
 }
