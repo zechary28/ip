11a61d90f6472236ab716a34661574663b873c33
zechary28 <zechary28@gmail.com>
2025-01-25 02:37:10 +0800

    refactor handle Task types

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -2,12 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Luke {
 
-    public static Task[] list;
+    public static ArrayList<Task> list;
     public static int numItems;
 
+    // abstract class
     public static abstract class Task {
         protected String name;
         protected boolean isDone;
@@ -23,6 +25,7 @@ public class Luke {
         public abstract String toString();
     }
 
+    // Task subtypes
     public static class ToDo extends Task {
 
         protected String dueDate;
@@ -81,16 +84,19 @@ public class Luke {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    // exceptions
+    public class InvalidInputException extends Exception {}
+
+    public static void main(String[] args) throws IOException, InvalidInputException {
         //io
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter writer = new PrintWriter(System.out);
 
         //data structures
-        list = new Task[100];
+        list = new ArrayList<>();
 
-        String logo = " _           _        \n"
-                + "| |    _   _| | _____ \n"
+        String logo = " _           _\n"
+                + "| |    _   _| | _____\n"
                 + "| |   | | | | |/ / _ \\\n"
                 + "| |___| |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
@@ -108,55 +114,92 @@ public class Luke {
             else if (inputArr[0].equals("list")) printList();
             else { // add tasks
                 String type = inputArr[0];
-                if (type.equals("todo")) list[numItems] = new ToDo(input.substring(5));
-                else if (type.equals("deadline")) {
-                    input = input.substring(9); // trip type
-                    inputArr = input.split(" /by ");
-                    String name = inputArr[0];
-                    String due = inputArr[1];
-                    list[numItems] = new Deadline(name, due);
+                if (type.equals("todo")) {
+                    handleToDo(input);
+                } else if (type.equals("deadline")) {
+                    handleDeadline(input);
                 } else if (type.equals("event")) {
-                    input = input.substring(6); // extract type
-                    inputArr = input.split(" /from "); input = inputArr[1];
-                    String name = inputArr[0]; // extract name
-                    inputArr = input.split(" /to ");
-                    String start = inputArr[0];
-                    String end = inputArr[1];
-                    list[numItems] = new Event(name, start, end);
+                    handleEvent(input);
                 }
-                printLine();
-                System.out.println("added: " + list[numItems]);
-                numItems++;
-                System.out.println("Now you have " + numItems + "tasks in the list.");
-                printLine();
+                addTaskUpdates();
             }
         }
     }
 
     public static void printLine() {
-        System.out.println("_____________________________________");
+
+        System.out.println("____________________________________________________________");
     }
 
     public static void exit() {
         printLine();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(" Bye. Hope to see you again soon!");
         printLine();
         System.exit(0);
     }
 
+    public static void handleToDo(String input) {
+        list.add(new ToDo(input.substring(5)));
+    }
+
+    public static void handleDeadline(String input) {
+        input = input.substring(9); // trip type
+        String[] inputArr = input.split(" /by ");
+        String name = inputArr[0];
+        String due = inputArr[1];
+        list.add(new Deadline(name, due));
+    }
+
+    public static void handleEvent(String input) {
+        input = input.substring(6); // extract type
+        String[] inputArr = input.split(" /from "); input = inputArr[1];
+        String name = inputArr[0]; // extract name
+        inputArr = input.split(" /to ");
+        String start = inputArr[0];
+        String end = inputArr[1];
+        list.add(new Event(name, start, end));
+    }
+
+    public static void addTaskUpdates() {
+        printLine();
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + list.get(numItems));
+        numItems++;
+        System.out.println(" Now you have " + numItems + " tasks in the list.");
+        printLine();
+    }
+
     public static void printList() {
+        printLine();
+        System.out.println(" Here are the tasks in your list:");
         for (int i = 0; i < numItems; i++) {
-            System.out.println(String.format("%d. %s",i+1, list[i]));
+            System.out.println(String.format(" %d.%s",i+1, list.get(i)));
         }
+        printLine();
     }
 
     public static void markTask(int i, boolean isDone) {
-        Task task = list[i-1];
+        Task task = list.get(i-1);
         task.setIsDone(isDone);
         printLine();
-        if (isDone) System.out.println("Nice! I've marked this task as done:");
-        else System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(task.toString());
+        if (isDone) {
+            System.out.println(" Nice! I've marked this task as done:");
+            System.out.println("   " + task.toString());
+            printLine();
+        } else {
+            System.out.println(" OK, I've marked this task as not done yet:");
+            System.out.println("   " + task.toString());
+            printLine();
+        }
+    }
+
+    public static void deleteTask(int i) {
+        Task task = list.remove(i);
+        printLine();
+        System.out.println(" Noted. I've removed this task:");
+        System.out.println("    " + task);
+        numItems--;
+        System.out.println(" Now you have " + numItems + " tasks in the list.");
         printLine();
     }
 
